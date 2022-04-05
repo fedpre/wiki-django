fb7f5b448a2f9ac6fd96d938b3bdd3a659a91513
fedpre <fed.pregnolato@gmail.com>
2022-04-05 14:49:25 +0200

    finished entry page & index page features

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from . import util
 
 from . import util
 
@@ -8,3 +10,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki_page(request, title):
+    wiki_page = util.get_entry(title)
+    if wiki_page == None:
+        raise Http404
+
+    return render(request, 'encyclopedia/wiki_page.html', {
+        "page": wiki_page,
+        "title": title,
+    })
